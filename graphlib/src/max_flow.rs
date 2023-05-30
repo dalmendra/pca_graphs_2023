05f1571b9a0e927cdc441752c18d6c086c672f8d
dalmendra <72281558+dalmendra@users.noreply.github.com>
2023-05-29 21:43:14 -0300

    Add files via upload

--- graphlib/src/max_flow.rs
+++ graphlib/src/max_flow.rs
@@ -3,7 +3,6 @@ use std::collections::{VecDeque};
 // Implementação do algoritmo de Edmonds-Karp para encontrar o fluxo máximo em um grafo
 pub fn edmonds_karp(graph: &Vec<Vec<i32>>, source: usize, sink: usize) -> i32 {
     let num_vertices = graph.len();
-    //let mut residual_graph = vec![vec![0; num_vertices]; num_vertices];
     let mut residual_graph = graph.clone();
     let mut parent = vec![None; num_vertices]; // Vetor que armazena o pai de cada vértice no augmenting path
     
@@ -30,18 +29,23 @@ pub fn edmonds_karp(graph: &Vec<Vec<i32>>, source: usize, sink: usize) -> i32 {
         v = sink;  // Começa pelo vértice de destino
         while v != source {
             let u = parent[v].unwrap();  // Obtém o pai
-            residual_graph[u][v] -= path_flow; // Reduz a capacidade residual da aresta original
+            residual_graph[u][v] -= path_flow; // Reduz a capacidade residual da aresta original            
             residual_graph[v][u] += path_flow; // Aumenta a capacidade residual da aresta reversa
             v = u; // Avança para o próximo vértice do augmenting path
         }
 
+        // Imprime o grafo residual (mostra as capacidades restantes de cada aresta - normal e reversa)
+        println!("Grafo residual a cada iteração:");
+        for k in 0..num_vertices {
+        println!("{:?}", residual_graph[k]);
+    }
         // Incrementar o fluxo máximo
         max_flow += path_flow;
     }
     
-    // Para debug - imprime o grafo residual (mostra as capacidades restantes de cada aresta - normal e reversa)
+    // Imprime o grafo residual (mostra as capacidades restantes de cada aresta - normal e reversa)
     println!("");
-    println!("Grafo residual:");
+    println!("Grafo residual final:");
     for k in 0..num_vertices {
         println!("{:?}", residual_graph[k]);
     }
@@ -60,15 +64,14 @@ pub fn bfs(graph: &Vec<Vec<i32>>, source: usize, sink: usize, parent: &mut Vec<O
 
     while !queue.is_empty() {
         let u = queue.pop_front().unwrap();
-
         for v in 0..num_vertices {
-            // Só executa se o vértice tiver sido visitado e a capacidade da aresta for maior que 0
+            // Só executa se o vértice não tiver sido visitado e a capacidade da aresta for maior que 0
             if !visited[v] && graph[u][v] > 0 {
                 visited[v] = true;
                 parent[v] = Some(u);
                 queue.push_back(v);
             }
-        }
+        } 
     }
 
     visited[sink]
@@ -77,9 +80,48 @@ pub fn bfs(graph: &Vec<Vec<i32>>, source: usize, sink: usize, parent: &mut Vec<O
 #[cfg(test)]
 mod test_flow {
     use crate::max_flow::edmonds_karp;
+    
     #[test]
-    fn test_flow_edmondskarp() {
+    fn test_flow_edmondskarp_preto_ppt() {
         // Grafo de exemplo
+        let graph = vec![
+            vec![0, 10, 10, 0, 0, 0],
+            vec![0, 0, 0, 25, 0, 0],
+            vec![0, 0, 0, 0, 15, 0],
+            vec![0, 0, 0, 0, 0, 10],
+            vec![0, 6, 0, 0, 0, 10],
+            vec![0, 0, 0, 0, 0, 0],
+        ];
+
+        let source = 0;
+        let sink = 5;
+
+        let max_flow = edmonds_karp(&graph, source, sink);
+
+        println!("Fluxo máximo: {}", max_flow);
+    }
+
+    #[test]
+    fn test_flow_edmondskarp_losango() {
+        // Grafo de exemplo
+        let graph = vec![
+            vec![0, 100, 100, 0],
+            vec![0, 0, 1, 100],
+            vec![0, 0, 0, 100],
+            vec![0, 0, 0, 0],
+        ];
+
+        let source = 0;
+        let sink = 3;
+
+        let max_flow = edmonds_karp(&graph, source, sink);
+
+        println!("Fluxo máximo: {}", max_flow);
+    }
+
+    #[test]
+    fn test_flow_edmondskarp_livro() {
+        // Grafo igual ao do livro do Cormen
         let graph = vec![
             vec![0, 16, 13, 0, 0, 0],
             vec![0, 0, 0, 12, 0, 0],
@@ -91,26 +133,6 @@ mod test_flow {
 
         let source = 0;
         let sink = 5;
-
-        let max_flow = edmonds_karp(&graph, source, sink);
-
-        println!("Fluxo máximo: {}", max_flow);
-    }
-    #[test]
-    fn test_flow_edmondskarp1() {
-        // Grafo de exemplo
-        let graph = vec![
-            vec![0, 3, 0, 3, 0, 0, 0],
-            vec![0, 0, 4, 0, 0, 0, 0],
-            vec![0, 0, 0, 1, 2, 0, 0],
-            vec![0, 0, 0, 0, 2, 6, 0],
-            vec![0, 0, 0, 0, 0, 0, 1],
-            vec![0, 0, 0, 0, 0, 0, 9],
-            vec![0, 0, 0, 0, 0, 0, 0],
-        ];
-
-        let source = 0;
-        let sink = 6;
 
         let max_flow = edmonds_karp(&graph, source, sink);
 
